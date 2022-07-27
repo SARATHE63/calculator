20497b09f82e5493f4865300d84b058cf2718f53
SARATHE63 <109621006+SARATHE63@users.noreply.github.com>
2022-07-27 10:38:18 +0530

    my first code
    THEY ARE NOT RUN SUCCESSFULLY .PLEASE HELP ME WAHT THE ERROR OCCUR.

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+   import java.until.*;
+ class Calculator {
+  
+  public static void main ( String [] args ) {
+   
+     Scanner sc = new Scanner(System.in);
+     char  operator;
+     System.out.println("Choose an operator :  + ,  *  ,  /  , -   "  );
+     operator=sc.next().charAt(0);
+   
+         Double num1 , num2,result ;
+         System.out.println("Enter first number:");
+         num1 =sc.nextDouble();
+         System.out.println("Enter second number:");
+         num2 =sc.nextDouble();
+
+     switch(operator)
+ {
+     case '+' : result  = num1 + num2;
+                    
+                  break;
+           case '*' : result = num1 - num2;
+                    
+                    break;  
+          case   '/'  : result = num1 * num2;
+                    
+                    break;
+            case '-'  : result = num1 / num2;
+                    
+                   break;
+            default : 
+                          System.out.println("wrong try again "); 
+              break;
+
+     } 
+       sc.close();
+          }
+
+                        }
